836c175aa70541865a46e943ce285777ad83f943
yorghet <1392696@senati.pe>
2023-04-04 12:01:34 -0500

    Listar MVC terminado

--- controllers/curso.controller.php
+++ controllers/curso.controller.php
@@ -9,7 +9,34 @@ if(isset($_POST['operacion'])){
   if($_POST['operacion'] == 'listar'){
 
     $datosObtenidos = $curso->listarCursos();
-    echo json_encode($datosObtenidos);
+    
+    //En esta ocación NO enviaremos un objeto JSON, en su lugar el controlador redenrizará las filas que necesita <tbody></tbody>
+    //echo json_encode($datosObtenidos);
+
+    // PASO 1: Verificar que el objeto contenga datos
+    if($datosObtenidos){
+      $numeroFila = 1;
+      // PASO 2. Recorrer todo el objeto
+      foreach($datosObtenidos as $curso){
+        // PASO 3: Ahora construimos las filas
+        echo "
+          <tr>
+            <td>{$numeroFila}</td>
+            <td>{$curso['nombrecurso']}</td>
+            <td>{$curso['especialidad']}</td>
+            <td>{$curso['complejidad']}</td>
+            <td>{$curso['fechainicio']}</td>
+            <td>{$curso['precio']}</td>
+            <td>
+            <a href='#' class='btn btn-danger btn-sm'><i class='bi bi-trash3-fill'></i></a>
+                <a href='#' class='btn btn-info btn-sm'><i class='bi bi-pencil-fill'></i></a>
+            </td>
+          </tr>
+        ";
+          $numeroFila++;
+      } // FIN DEL FOREACH
+    }
 
   }
+
 }
