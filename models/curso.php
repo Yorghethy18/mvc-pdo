515ee455c5e290ed827de48f818f92ee8a38f028
yorghet <1392696@senati.pe>
2023-04-04 10:34:43 -0500

    Métod listar terminado en curso

--- models/curso.php
+++ models/curso.php
@@ -6,17 +6,54 @@ require_once "./Conexion.php";
 // extends : HERENCIA (POO) en PHP
 class Curso extends Conexion{
 
-  // Objeto que almacena la conexión que viene desde el padre (Conexion)
+  // Objeto que almacena la conexión que viene desde el padre (Conexion) y la compartirá con todos los métodos (CRUD+)
   private $accesoBD;
 
   // Constructor
   public function __CONSTRUCT(){
-
+    $this->accesoBD = parent::getConexion(); //El valor de retorno de esta funcion ha sido asignada a este objeto. Si getConexion devuelve el retorno al acceso.
   }
   
    // Método listar cursos
    public function listarCursos(){
+    try {
+      // 1. Preparamos la consulta
+     $consulta = $this->accesoBD->prepare("CALL spu_curso_listar()");
+     // 2. Ejecutamos la consulta
+     $consulta->execute();
+     // 3. Devolvemos el resultado
+     return $consulta->fetchAll(PDO::FETCH_ASSOC);
+    } 
+    catch (Exception $e) {
+      die($e->getMessage());
+    }
+   }
 
+   public function registrarCurso(){
+    try {
+      
+    } 
+    catch (Exception $e) {
+      die($e->getMessage());
+    }
+   }
+
+   public function eliminarCurso(){
+    try {
+      
+    } 
+    catch (Exception $e) {
+      die($e->getMessage());
+    }
+   }
+
+   public function actualizarCurso(){
+    try {
+      
+    } 
+    catch (Exception $e) {
+      die($e->getMessage());
+    }
    }
 
 }
