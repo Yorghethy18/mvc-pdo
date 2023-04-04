be05e6e13970e2be381aaff4454b25b3596ad2f1
yorghet <1392696@senati.pe>
2023-04-04 11:06:01 -0500

    Test OK proceso listar cursos

---
+++ controllers/curso.controller.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once '../models/Curso.php';
+
+if(isset($_POST['operacion'])){
+
+  $curso = new Curso();
+
+  if($_POST['operacion'] == 'listar'){
+
+    $datosObtenidos = $curso->listarCursos();
+    echo json_encode($datosObtenidos);
+
+  }
+}
